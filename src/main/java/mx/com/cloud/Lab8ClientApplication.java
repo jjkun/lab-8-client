df205aeaddb92b5f4670ebe29ef2fa8c1906f096
Juan J. Perea <jjperea@intranet.proconsultingpartners.com>
2018-11-08 17:03:42 -0600

    first commit

---
+++ src/main/java/mx/com/cloud/Lab8ClientApplication.java
@@ -0,0 +1,12 @@
+package mx.com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab8ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab8ClientApplication.class, args);
+	}
+}
